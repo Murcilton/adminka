10b85a283cad7d6c681d3e0ad6a37a73cc13127e
Murcilton <studkab5@jdk.ru>
2024-03-06 14:56:05 +0500

    first commit

---
+++ app/Http/Controllers/Admin/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index()
+    {
+        $tag = new Tag();
+        $tag->title = 'Хай';
+        $tag->save();
+        return view ('admin.index');
+    }
+}
